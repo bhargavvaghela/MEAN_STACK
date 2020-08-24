78037bbb0eef6395fe8719616e82197852e76904
bhargavvaghela <45867652+bhargavvaghela@users.noreply.github.com>
2020-08-25 00:40:27 +0530

    Add files via upload

---
+++ day6/main.js
@@ -0,0 +1,19 @@
+const PI = require("./my-mod/my.module1");
+const mod2 = require("./my-mod/my.module2");
+const mod3 = require("./my-mod/my.module3");
+const mod4 = require("./my-mod/my.module4");
+
+
+console.log(PI);
+console.log("I am main function");
+
+
+console.log(mod2);
+console.log(mod2.MY_APP_NAME);
+console.log(mod2.MY_AREA);
+
+console.log(mod3);
+
+
+console.log(mod4);
+console.log(module);
